21d1e4025eff92c9a5250980d8a5bb20bc889531
Sergey Shkurov <sergey.shkurov@wolt.com>
2025-12-04 01:39:09 +0100

    Day 3 Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,35 +1,43 @@
+import kotlin.math.pow
+
 fun main() {
 
     fun parseBank(line: String): List<Int> {
         return line.map { it.digitToInt() }
     }
 
-    fun part1(input: List<String>): Int {
-        fun solve(line: String): Int {
-            val bank = parseBank(line)
+    fun solve(bank: List<Int>, remainingDigits: Int): Long {
+        val (maxIndex, maxValue) = bank.dropLast(remainingDigits - 1)
+            .withIndex()
+            .maxBy { it.value }
 
-            val (firstIndex, firstValue) = bank.dropLast(1)
-                .withIndex()
-                .maxBy { it.value }
-
-            val secondValue = bank.drop(firstIndex + 1).max()
-
-            return firstValue * 10 + secondValue
+        return if (remainingDigits == 1) {
+            maxValue.toLong()
+        } else {
+            val scaledValue = maxValue * 10.0.pow(remainingDigits - 1).toLong()
+            val remaining = bank.subList(maxIndex + 1, bank.size)
+            scaledValue + solve(remaining, remainingDigits - 1)
         }
-
-        return input.sumOf(::solve)
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part1(input: List<String>): Long {
+        return input
+            .map { parseBank(it) }
+            .sumOf { solve(it, 2) }
+    }
+
+    fun part2(input: List<String>): Long {
+        return input
+            .map { parseBank(it) }
+            .sumOf { solve(it, 12) }
     }
 
     val testInput = readInput("Day03_test")
     val input = readInput("Day03")
 
-    check(part1(testInput) == 357)
+    check(part1(testInput) == 357L)
     part1(input).println()
 
-    check(part2(testInput) == 123)
+    check(part2(testInput) == 3121910778619L)
     part2(input).println()
 }
