2ba3e10b0becbcf12afc14eba86361446cec409c
Sergey Shkurov <seryoga6531@gmail.com>
2026-02-28 03:08:26 +0100

    Day 11 Part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -6,31 +6,62 @@ fun main() {
             .associate { (from, to) -> from to to.split(" ") }
     }
 
-    fun countPaths(connections: Map<String, List<String>>, from: String): Long {
-        if (from == "out") {
-            return 1
+    fun countPaths(
+        connections: Map<String, List<String>>,
+        from: String,
+        to: String,
+        predicate: (PathContext) -> Boolean = { true }
+    ): Long {
+        val cache = mutableMapOf<PathContext, Long>()
+
+        fun step(context: PathContext): Long {
+            val next = connections[context.node] ?: return 0
+
+            return cache.getOrPut(context) {
+                next.sumOf { nextPath ->
+                    val nextContext = PathContext(
+                        node = nextPath,
+                        dacSeen = context.dacSeen || nextPath == "dac",
+                        fftSeen = context.fftSeen || nextPath == "fft"
+                    )
+
+                    if (nextPath == to) {
+                        if (predicate(nextContext)) {
+                            1
+                        } else {
+                            0
+                        }
+                    } else {
+                        step(nextContext)
+                    }
+                }
+            }
         }
 
-        val next = connections[from] ?: return 0
-        return next.sumOf { countPaths(connections, it) }
+        return step(PathContext(from, dacSeen = false, fftSeen = false))
     }
 
     fun part1(input: List<String>): Long {
         val connections = parseConnections(input)
 
-        return countPaths(connections, "you")
+        return countPaths(connections, "you", "out")
     }
 
     fun part2(input: List<String>): Long {
-        TODO()
+        val connections = parseConnections(input)
+
+        return countPaths(connections, "svr", "out") { (_, dacSeen, fftSeen) -> dacSeen && fftSeen }
     }
 
     val testInput = readInput("Day11_test")
+    val testInput2 = readInput("Day11_test2")
     val input = readInput("Day11")
 
     check(part1(testInput) == 5L)
     part1(input).println()
 
-    check(part2(testInput) == 0L)
+    check(part2(testInput2) == 2L)
     part2(input).println()
 }
+
+private data class PathContext(val node: String, val dacSeen: Boolean, val fftSeen: Boolean)
